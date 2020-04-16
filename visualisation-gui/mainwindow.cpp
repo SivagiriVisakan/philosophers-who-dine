ee0f4da9875773273bb6124fe955b0120d453492
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-04-17 02:25:33 +0530

    qt: Use the shared memory to update the UI accordingly as per the state in dining room.

--- visualisation-gui/mainwindow.cpp
+++ visualisation-gui/mainwindow.cpp
@@ -6,8 +6,11 @@
 #include <QDesktopWidget>
 #include <QDebug>
 #include <QPushButton>
+#include <QTimer>
 #include <cmath>
-
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
 // TODO: Remove this or make is meaningful
 // Note: This is all for debug right now.
 void MainWindow::onButtonClick()
@@ -20,7 +23,38 @@ void MainWindow::onButtonClick()
     moveForkToPhilosopher(1, 1);
 
     forks[1]->moveToTable();
+    qInfo() << philosophers_state_info[0].fork_held;
 
+}
+
+void MainWindow::checkForStatusUpdates()
+{
+
+    char *state_messages[] = {
+        "Eating",
+        "Thinking",
+        "Waiting for left fork",
+        "Waiting for right fork"
+    };
+
+    qInfo() << state_messages[philosophers_state_info[0].current_state];
+    for(int i=0; i < TOTAL_PHILOSOPHERS; i++)
+    {
+        Fork *f = forks[i];
+        int ownerIndex = forks_state_info[i].owner_id;
+        qInfo() << " Fork " << i << ": "<<forks_state_info[i].owner_id;
+        if(ownerIndex == -1)
+        {
+            f->moveToTable();
+        }
+        else {
+            int differenceRange =  i % 2 == 0? 5:0; // The difference in degrees
+            int r = 280;
+            QPoint end = calculatePoint(r, philosopherDegrees[ownerIndex] + differenceRange, f);
+            f->moveToPoint(end);
+
+        }
+    }
 }
 
 QPoint MainWindow::calculatePoint(int radius, double degree, const QWidget *w)
@@ -93,11 +127,39 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(onButtonClick()));
 
-    this->centerPoint = ui->centerPoint->pos();
     setupDiningRoom();
+    initSharedMemory();
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(checkForStatusUpdates()));
+    timer->start(2000);
+
 }
 
+void MainWindow::initSharedMemory()
+{
+    key_t philosophers_state_key = 567;
+    key_t forks_state_key = 568;
 
+   // shmget returns an identifier in shmid
+   int shmid = shmget(philosophers_state_key, sizeof(struct philosopher_state) * TOTAL_PHILOSOPHERS, 0666);
+   if (shmid < 0) {
+        printf("*** shmget error (can't acquire for philosopher's state) ***\n");
+        exit(1);
+   }
+    philosophers_state_info = (struct philosopher_state *) shmat(shmid,NULL,0);
+
+   // shmget returns an identifier in shmid
+   shmid = shmget(forks_state_key, sizeof(struct fork_info) * TOTAL_PHILOSOPHERS, 0666|IPC_CREAT);
+   if (shmid < 0) {
+        printf("*** shmget error (can't acquire for fork's state) ***\n");
+        exit(1);
+   }
+    forks_state_info = (struct fork_info *) shmat(shmid,NULL,0);
+
+
+
+}
 void MainWindow::drawForksAndFood()
 {
     int r = foodAndForksRadius;
@@ -150,7 +212,9 @@ void MainWindow::drawForksAndFood()
 void MainWindow::setupDiningRoom()
 {
     ui->table->move(width() / 2 - ui->table->width() / 2, height() / 2 - ui->table->height() / 2);
+
     ui->centerPoint->move(width() / 2 - ui->centerPoint->width() / 2, height() / 2 - ui->centerPoint->height() / 2);
+    this->centerPoint = ui->centerPoint->pos();
 
     drawForksAndFood();
     drawPhilosophers();
@@ -160,6 +224,9 @@ void MainWindow::setupDiningRoom()
 
 MainWindow::~MainWindow()
 {
+    //detach from shared memory
+    shmdt(philosophers_state_info);
+
     delete ui;
 }
 
--- visualisation-gui/mainwindow.h
+++ visualisation-gui/mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <QLabel>
 #include "fork.h"
+#include "../philsophers_base.h"
+
+#define TOTAL_PHILOSOPHERS 5
+
 namespace Ui {
 class MainWindow;
 }
@@ -18,6 +22,8 @@ public:
 
 public slots:
     void onButtonClick();
+    void checkForStatusUpdates();
+
 private:
     Ui::MainWindow *ui;
     Fork *forks[5];
@@ -25,6 +31,10 @@ private:
     QLabel *philosophers[5];
     QPoint centerPoint; // The center of the screen.
     int philosopherDegrees[5]; //  The degree relative to center at which the philosophers are placed.
+
+    struct philosopher_state *philosophers_state_info;
+    struct fork_info *forks_state_info;
+
     const int philosophersRadius = 250; // The radius from the center of the table at which philosophers are placed.
     const int foodAndForksRadius = 120; // The radius from the center of the table at which the forks and food plates are placed.
 
@@ -33,6 +43,7 @@ private:
     void drawForksAndFood();
     void drawPhilosophers();
     QPoint calculatePoint(int radius, double degree, const QWidget *w=nullptr);
+    void initSharedMemory();
 };
 
 #endif // MAINWINDOW_H
