15e83b1f6f299f2f951cce60ddcee9a5483ed312
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-04-16 18:14:23 +0530

    Initialize empty QT project for visualisation of the dining philosophers.

---
+++ visualisation-gui/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QStyle>
+#include <QDesktopWidget>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setWindowFlags(Qt::Drawer);
+    setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            size(),
+            qApp->desktop()->availableGeometry()
+        ));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
